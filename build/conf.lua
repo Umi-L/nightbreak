3a8dfbe8b4f989cefacd05c2e41e03f07fc4b9be
umi <julianogrady07@gmail.com>
2022-04-16 14:51:47 -0400

    added .gitignore

--- build/conf.lua
+++
@@ -1,4 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-love.conf = function(t)
-    t.console = true
-end
--- build/libraries/TiledParser.lua
+++
@@ -1,7 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-local map = require("maps.untitled")
-function ____exports.TiledDebug(self)
-    print(map.height)
-end
-return ____exports
